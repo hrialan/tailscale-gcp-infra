205b8e138311aa8cf5ff96965ff7f020c2a9daeb
Hugo <hugo.rialan@devoteamgcloud.com>
2024-07-17 13:38:30 +0000

    Update

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+  backend "gcs" {
+    bucket = "dgc-sandbox-hrialan-terraform"
+    prefix = "tailscale/"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0.0, <6.0.0"
+    }
+  }
+}
 
 provider "google" {
   project = var.project
